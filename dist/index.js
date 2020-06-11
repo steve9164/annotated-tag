5d1329ff9d827793502f847d2768bea69d2398ca
Stephen Davies <steve9164@gmail.com>
2020-06-11 23:11:51 +1000

    Tag only releases, add tag author options and bump version

--- dist/index.js
+++ dist/index.js
@@ -2431,12 +2431,20 @@ function run() {
         try {
             const tagName = core.getInput("tag-name", { required: true });
             const tagMessage = core.getInput("tag-message");
+            const taggerName = core.getInput("tagger-name") || undefined;
+            const taggerEmail = core.getInput("tagger-email") || undefined;
             const token = core.getInput("github-token", { required: true });
             const octokit = github.getOctokit(token);
             if (tagMessage) {
                 core.info("Creating an annotated git tag equivalent to:");
                 core.info(`  git tag -a ${tagName} -m "${tagMessage}"`);
-                const tagRequest = yield octokit.git.createTag(Object.assign(Object.assign({}, github.context.repo), { tag: tagName, message: tagMessage, object: github.context.sha, type: "commit" }));
+                if (taggerName || taggerEmail) {
+                    core.info(`Using tag author: ${taggerName || ""}${taggerEmail ? ` <${taggerEmail}>` : ""}`);
+                }
+                const tagRequest = yield octokit.git.createTag(Object.assign(Object.assign({}, github.context.repo), { tag: tagName, message: tagMessage, object: github.context.sha, type: "commit", tagger: {
+                        name: taggerName,
+                        email: taggerEmail,
+                    } }));
                 yield octokit.git.createRef(Object.assign(Object.assign({}, github.context.repo), { ref: `refs/tags/${tagName}`, sha: tagRequest.data.sha }));
             }
             else {
--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,30 @@ async function run(): Promise<void> {
   try {
     const tagName = core.getInput("tag-name", { required: true });
     const tagMessage = core.getInput("tag-message");
+    const taggerName = core.getInput("tagger-name") || undefined;
+    const taggerEmail = core.getInput("tagger-email") || undefined;
     const token = core.getInput("github-token", { required: true });
     const octokit = github.getOctokit(token);
     if (tagMessage) {
       core.info("Creating an annotated git tag equivalent to:");
       core.info(`  git tag -a ${tagName} -m "${tagMessage}"`);
+      if (taggerName || taggerEmail) {
+        core.info(
+          `Using tag author: ${taggerName || ""}${
+            taggerEmail ? ` <${taggerEmail}>` : ""
+          }`
+        );
+      }
       const tagRequest = await octokit.git.createTag({
         ...github.context.repo,
         tag: tagName,
         message: tagMessage,
         object: github.context.sha,
         type: "commit",
+        tagger: {
+          name: taggerName,
+          email: taggerEmail,
+        },
       });
       await octokit.git.createRef({
         ...github.context.repo,
